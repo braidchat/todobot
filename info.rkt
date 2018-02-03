4a7cca2c33e049b6d1907adbf885ea1dc0f0e559
Sam Juraschka <samjuraschka@gmail.com>
2018-02-03 12:58:46 -0500

    Update to use newest braidbot library
    We need to use the UUID->string function

--- info.rkt
+++ info.rkt
@@ -1,9 +1,9 @@
 #lang info
 (define collection "todobot")
 (define deps '("base"
-               "https://github.com/braidchat/braidbot.git#v1.1"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/todobot.scrbl" ())))
+               "https://github.com/braidchat/braidbot.git#v2.1"))
+(define build-deps '("racket-doc" "rackunit-lib"))
+(define scribblings '())
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(SJ))
